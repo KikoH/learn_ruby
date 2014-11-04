02c417b43269f64a881777e55d5bd45dbf5f240a
Kiko <mmhabib11@gmail.com>
2014-11-03 19:55:41 -0500

    Book titles test completed

---
+++ 07_book_titles/book.rb
@@ -0,0 +1,19 @@
+class Book
+	attr_reader :title
+
+	def capitalize(string)
+	lower_case = %w{the a an and in of }
+	words = string.split(' ')
+	words.map do |word|
+		unless lower_case.include? word
+			word.capitalize!
+		end
+		end
+		words[0].capitalize!
+		words.join(' ')
+	end
+
+	def title=(string)
+		@title = capitalize(string)
+	end
+end
