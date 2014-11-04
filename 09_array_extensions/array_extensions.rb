a8425c54be0aecfaaa9e1e50e6102834a9668e9d
Kiko <mmhabib11@gmail.com>
2014-11-03 23:42:00 -0500

    Array extensions test completed

--- 09_array_extensions/array_extensions.rb
+++ 09_array_extensions/array_extensions.rb
@@ -1,13 +1,13 @@
 class Array
-
+	#
 	def sum
 		self.inject(0) {|x,y| x + y}
 	end
-
+	#Creates a new array
 	def square
 		self.map {|x| x ** 2}
 	end
-
+	#Replaces existing array because of !
 	def square!
 		self.map! {|x| x ** 2}
 	end
